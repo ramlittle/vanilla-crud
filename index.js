ad5cf1a43d2f09b7104bcb24b516e8409a43ebbd
Ramelito Martinez <ramelitomartinez@gmail.com>
2023-05-22 12:58:30 +0800

    feat: made vanilla CRUD

--- index.js
+++ index.js
@@ -1,79 +1,64 @@
 const Form =document.querySelector('#form');
 const Task = document.querySelector('#task');
-const Date = document.querySelector('#date');
-const Msg = document.querySelector('#msg');
-const Posts = document.querySelector('#posts');
+const Date = document.querySelector('#date')
+const Msg = document.querySelector('#msg')
+const Tasks=document.querySelector('#tasks')
 
-let data=[];
+let data = [];
 
 Form.addEventListener('submit',(e)=>{
     e.preventDefault();
-    formValidation();
-})
-
-function formValidation(){
     if(!!!Task.value || !!!Date.value){
-        console.log('Please put Task and date');
-        Msg.innerHTML = 'Task must not be empty'
+        console.log('please fill out the form')
+        Msg.innerHTML = 'Please complete details'
     }else{
-        console.log('success entry is there now');
         acceptData();
     }
-}
-
-// ADD DATA IN LOCAL STORAGE
-function acceptData(){
-    data.push({
-        task:Task.value,
-        date:Date.value
-    });
-    console.log(data);
-
-    localStorage.setItem('data',JSON.stringify(data));
-    createTasks();
-}
-
-// DISPLAY DATA IN FRONTEND
-
-function createTasks(){
-    Posts.innerHTML='';
-    data.map((x,y)=>{
-    return(
-        Posts.innerHTML += `
-        <div id = '${y}'>
-            <span>${x.task}</span>
-            <span>${x.date}</span>
-            <span>
-                <i onclick='editTask(this)'>edit</i>
-                <i onclick='deleteTask(this)'>delete</i>
-            </span>
-        </div>
-        `
-    )
 })
-resetForm();
+
+function acceptData(){
+    data.push(
+        {
+            task:Task.value,
+            date:Date.value
+        }
+    )
+    console.log(data);
+    displayData();
+}
+function displayData(){
+    Tasks.innerHTML='';
+    data.map((x,y)=>{
+        return(
+            Tasks.innerHTML += `
+                <div id = '${y}'>
+                    <span>${x.task}</span>
+                    <span>${x.date}</span>
+                    <span>
+                        <i onclick='editTask(this)'>edit</i>
+                        <i onclick='deleteTask(this)'>delete</i>
+                    </span>
+                </div>
+            `
+        )
+    })
+    clearForm();
 }
 
-function resetForm(){
-    Task.value ='';
-    Date.value='';
+function clearForm(){
+    Task.value=''
+    Date.value=''
 }
 
-// DELETE DATA
-let deleteTask=(e)=>{
-    e.parentElement.parentElement.remove();
-    data.splice(e.parentElement.parentElement.id, 1);
-    localStorage.setItem('data',JSON.stringify(data));
-    console.log(data)
-}
-
-// EDIT DATA
 let editTask=(e)=>{
     let selectedTask=e.parentElement.parentElement;
-
-    Task.value = selectedTask.children[0].innerHTML;
+    Task.value=selectedTask.children[0].innerHTML;
     Date.value=selectedTask.children[1].innerHTML;
-
     deleteTask(e);
 }
 
+let deleteTask=(e)=>{
+    e.parentElement.parentElement.remove();
+    data.splice(e.parentElement.parentElement.id,1)
+    console.log(data)
+}
